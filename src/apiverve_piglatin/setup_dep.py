d0a2b092568c780082e4139c1a9866e2d8cca45a
APIVerve Team <hello@apiverve.com>
2025-03-28 17:40:36 -0500

    Bug Fixes

---
+++ src/apiverve_piglatin/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_piglatin',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Pig Latin is a simple tool for encoding Pig Latin. It returns the encoded Pig Latin.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
